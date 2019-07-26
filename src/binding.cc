bf3eb7f0b2cf02482b3327bd220e21ca473848e7
Benjamin Gray <bengray178@gmail.com>
2019-07-26 11:41:02 +1000

    start NAPI conversion

--- src/binding.cc
+++ src/binding.cc
@@ -1,6 +1,5 @@
+#include "napi.h"
 #include "tree_sitter/parser.h"
-#include <node.h>
-#include "nan.h"
 
 using namespace v8;
 
@@ -8,21 +7,29 @@ extern "C" TSLanguage * tree_sitter_biber();
 
 namespace {
 
-NAN_METHOD(New) {}
+Napi::Object Init(Napi::Env env, Napi::Object exports) {
 
-void Init(Local<Object> exports, Local<Object> module) {
-  Local<FunctionTemplate> tpl = Nan::New<FunctionTemplate>(New);
-  tpl->SetClassName(Nan::New("Language").ToLocalChecked());
-  tpl->InstanceTemplate()->SetInternalFieldCount(1);
 
-  Local<Function> constructor = Nan::GetFunction(tpl).ToLocalChecked();
-  Local<Object> instance = constructor->NewInstance(Nan::GetCurrentContext()).ToLocalChecked();
-  Nan::SetInternalFieldPointer(instance, 0, tree_sitter_biber());
 
-  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("biber").ToLocalChecked());
-  Nan::Set(module, Nan::New("exports").ToLocalChecked(), instance);
+  return exports;
 }
 
-NODE_MODULE(tree_sitter_biber_binding, Init)
+NODE_API_MODULE(NODE_GYP_MODULE_NAME, Init)
 
-}  // namespace
+
+
+// void Init(Local<Object> exports, Local<Object> module) {
+//   Local<FunctionTemplate> tpl = Nan::New<FunctionTemplate>(New);
+//
+//
+//
+//   tpl->SetClassName(Nan::New("Language").ToLocalChecked());
+//   tpl->InstanceTemplate()->SetInternalFieldCount(1);
+//
+//   Local<Function> constructor = Nan::GetFunction(tpl).ToLocalChecked();
+//   Local<Object> instance = constructor->NewInstance(Nan::GetCurrentContext()).ToLocalChecked();
+//   Nan::SetInternalFieldPointer(instance, 0, tree_sitter_biber());
+//
+//   Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("biber").ToLocalChecked());
+//   Nan::Set(module, Nan::New("exports").ToLocalChecked(), instance);
+// }
